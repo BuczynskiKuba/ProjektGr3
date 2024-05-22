1e542279a846aa9e30e5c957971ca58dc1dd74be
Nasakrator <jakub.batko06@gmail.com>
2024-05-22 12:01:49 +0200

    switche i elsy do tabeli
    teraz tylko obrazdki dodac i bedzie baja
    no i oczywiscie css

--- kod/script.js
+++ kod/script.js
@@ -1,3 +1,4 @@
+// url
 const server = 'http://localhost:8080/radios/';
 
 
@@ -22,12 +23,12 @@ const getData = async (url) => {
     }
 }
 
+// taki Main, tutaj sie wykonuja rzeczy w petli co 10s
 const loop = async () => {
+    // pobranie danych 
     let data = await getData(server);
-    if(data){
-        console.log(data);
-    }
 
+    // dodawanie tabeli do html'a
     table.innerHTML = tableGenerator(data)
 
 
@@ -37,12 +38,17 @@ const loop = async () => {
 const intervalId = setInterval(loop, 1000)
 
 
-
+// pobranie z html diva o klasie .table gdzie wrzuca sie tabela
 const table = document.querySelector('.table');
 
 
+
 const tableGenerator = (data) => {
+    // id wierszy tabeli
     let deviceId = 0;
+
+    // html tabeli
+
     let html = `
         <table>
             <tr>
@@ -53,37 +59,93 @@ const tableGenerator = (data) => {
                 <th>Strenth</th>
                 <th>Battery Level</th>
                 <th>Working Mode</th>
+                <th>Position Lat</th>
+                <th>Position Lon</th>
             </tr>
     `;
 
+
+    // petla for po pobranym obiekcie z danymi
     data.forEach(element => {
-
+        
+        // w tych zmiennych jest ustalana sciezka do odpowiedniego obrazka
         let type = '';
+        let strength = '';
+        let batteryLevel = ''
+        let workingMode = '';
 
+
+        // ponizsze if'y i switche zmieniaja sciezki do zdjec
         switch(element.Type){
             case "Portable":
                 type = 'images/Portable.png'
                 break
             case "Car":
                 type = 'images/Car.png'
+                break
+            case "BaseStation":
+                type = "images/BaseStation.png";
+                break
+            default:
+                type = "images/Unknown.png"
+                break
+        }
+        
+        if( element.Strength > 0 && element.Strength < 3){
+            strength = 'images/Strength1.png'
+        }else if( element.Strength >= 3 && element.Strength < 5){
+            strength = 'images/Strength2.png';
+        }else if( element.Strength >= 5 && element.Strength < 7){
+            strength = 'images/Strength3.png';
+        }else if( element.Strength >= 7){
+            strength = 'images/Strength4.png';
+        }else{
+            strength = 'images/Strength0.png';
         }
 
+        if(element.BatteryLevel > 0 && element.BatteryLevel < 25){
+            batteryLevel = 'images/Battery1.png'
+        }else if(element.BatteryLevel >= 25 && element.BatteryLevel < 50){
+            batteryLevel = 'images/Battery2.png'
+        }else if(element.BatteryLevel >= 50 && element.BatteryLevel < 75){
+            batteryLevel = 'images/Battery3.png'
+        }else if(element.BatteryLevel >= 75){
+            batteryLevel = 'images/Battery4.png'
+        }else{
+            batteryLevel = 'images/Battery0.png'
+        }
+
+        switch(element.WorkingMode){
+            case "Voice":
+                WorkingMode = 'images/Voice.png'
+                break
+            case "Data":
+                WorkingMode = 'images/Data.png'
+                break
+            case "Idle":
+                WorkingMode = 'images/Idle.png'
+            default:
+                WorkingMode = 'images/Unknown.png'
+        }
+
+
+        // dodawanie kolejnych wierszy do tabeli
         html += `
             <tr id='deviceId ` + deviceId + `'>
                 <td>` + element.Id + `</td>
                 <td>` + element.Name + `</td>
-                <td>` + element.Type + `</td>
+                <td><img scr='` + type + `'/></td>
                 <td>` + element.SerialNumber + `</td>
-                <td>` + element.Strength + `</td>
-                <td>` + element.BatteryLevel + `</td>
-                <td>` + element.WorkingMode + `</td>
+                <td><img scr='` + strength + `'/></td>
+                <td><img scr='` + batteryLevel + `'/></td>
+                <td><img scr='` + workingMode + `'/></td>
+                <td>` + element.Position.Lat + `</td>
+                <td>` + element.Position.Lon + `</td>
             </tr>
         `
+        // inkrementacja id wiersza 
         deviceId++;
     });
-
-
-
 
     html += '</table>'
 
