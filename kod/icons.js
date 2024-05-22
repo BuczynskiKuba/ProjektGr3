7a920397408a29d6cc81a2c541618b51c2bc2e77
Maxicom0001 <karate.lynx@gmail.com>
2024-05-22 12:45:50 +0200

    fixed?

--- kod/icons.js
+++ kod/icons.js
@@ -1,23 +1,23 @@
 var baseStation = L.icon({
-    iconUrl: 'custom-marker.png',
+    iconUrl: '../res/custom-marker.png',
     iconSize: [25, 41], // size of the icon
     iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
     popupAnchor: [-3, -41] // point from which the popup should open relative to the iconAnchor
 });
 var car = L.icon({
-    iconUrl: 'custom-marker.png',
+    iconUrl: '../res/custom-marker.png',
     iconSize: [25, 41], // size of the icon
     iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
     popupAnchor: [-3, -41] // point from which the popup should open relative to the iconAnchor
 });
 var portable = L.icon({
-    iconUrl: 'custom-marker.png',
+    iconUrl: '../res/custom-marker.png',
     iconSize: [25, 41], // size of the icon
     iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
     popupAnchor: [-3, -41] // point from which the popup should open relative to the iconAnchor
 });
 var unknown = L.icon({
-    iconUrl: 'custom-marker.png',
+    iconUrl: '../res/custom-marker.png',
     iconSize: [25, 41], // size of the icon
     iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
     popupAnchor: [-3, -41] // point from which the popup should open relative to the iconAnchor
